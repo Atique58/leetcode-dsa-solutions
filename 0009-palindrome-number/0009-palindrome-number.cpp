fd63a7c9395ec1effcf0d01dbcbf760efb8afd6e
Atique Ur Rehman <atiqurehman2007@gmail.com>
2026-07-24 22:58:39 +0500

    Time: 0 ms (100%), Space: 8.6 MB (65.66%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        int orig=x;
+		if(x<0)
+		{
+			return false;
+		}
+		long reverse=0;
+		while(x>0)
+		{
+			int digit=x%10;
+			reverse=reverse*10+digit;
+			x=x/10;
+		}
+		return orig == reverse;         
+    }
+};
